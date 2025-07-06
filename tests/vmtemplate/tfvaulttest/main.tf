c7e8cadc9a07c0b63a6a75409c3d031739d43f68
Patrick Hermann <patrick.hermann@sva.de>
2025-07-06 06:07:08 +0000

    feat: feat/add-git-vm

---
+++ tests/vmtemplate/tfvaulttest/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = var.vault_addr
+
+  auth_login {
+    path = "auth/approle/login"
+    parameters = {
+      role_id   = var.vault_role_id
+      secret_id = var.vault_secret_id
+    }
+  }
+}
+
+# For KV v2, include `/data/` in the path
+data "vault_kv_secret_v2" "myapp" {
+  mount = "cloud"
+  name  = "vsphere"
+}

049232b04f7026a4bfb5907e4311a95ad14601e7
Patrick Hermann <patrick.hermann@sva.de>
2025-07-11 20:07:11 +0000

    feat: feat/add-k8s-microservice

---
+++ kubernetes-microservice/image.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"dagger/kubernetes-microservice/internal/dagger"
+)
+
+func (m *KubernetesMicroservice) BakeImage(
+	ctx context.Context,
+	// The source directory
+	src *dagger.Directory,
+	// The repository name
+	repositoryName string,
+	// tag
+	tag string,
+	// The registry username
+	// +optional
+	registryUsername *dagger.Secret,
+	// The registry password
+	// +optional
+	registryPassword *dagger.Secret,
+	// The registry URL
+	registryURL string,
+	// The Dockerfile path
+	// +optional
+	// +default="Dockerfile"
+	dockerfile string,
+	// Set extra directories
+	// +optional
+	withDirectories []*dagger.Directory,
+) (string, error) {
+
+	return dag.
+		Docker().
+		BuildAndPush(
+			ctx,
+			src,
+			repositoryName,
+			tag,
+			registryURL,
+			dagger.DockerBuildAndPushOpts{
+				Dockerfile:       dockerfile,
+				WithDirectories:  withDirectories,
+				RegistryUsername: registryUsername,
+				RegistryPassword: registryPassword,
+			},
+		)
+}

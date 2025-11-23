dd2790c0f57a5c4739cb1fc7ca63191c519aa8bc
Patrick Hermann <patrick.hermann@sva.de>
2025-11-23 12:12:14 +0000

    feat: feat/add-vm-config-generator

---
+++ configuration/vsphere_vm.go
@@ -0,0 +1,11 @@
+package main
+
+import "context"
+
+func (v *Configuration) VsphereVm(
+	ctx context.Context,
+	// VM t-shirt size: small, medium, large, or xlarge
+	size string,
+) (string, error) {
+
+}
